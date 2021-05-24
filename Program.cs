1bac57f4efdb0e9c11d0b1024f02da712478cc85
Sören Kuklau <chucker23n+github@gmail.com>
2021-05-24 13:36:20 +0200

    Initial implementation

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Foundation;
+using AppKit;
+
+namespace net6_console_mac_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // System.DllNotFoundException: Could not find the runtime library libxammac.dylib
+            NSApplication.Init();
+
+            // [ERROR] FATAL UNHANDLED EXCEPTION: System.TypeInitializationException: TypeInitialization_Type, Foundation.NSString
+            var str = new NSString("test");
+            var num = new NSNumber(123.45);
+
+            Console.WriteLine($"s has float value {num.FloatValue}");
+        }
+    }
+}
